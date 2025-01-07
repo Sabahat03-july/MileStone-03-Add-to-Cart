066ebf448767a7f7caa22839ca0c2f88cc07e1c9
Your Name <your.email@example.com>
2025-01-07 13:01:27 +0500

    now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from "./component/Navbar"
-import Footer from "./component/Footer"
+import Footer from "@/app/component/Footer"
 
 const inter = Inter({ subsets: ['latin'] })
 
